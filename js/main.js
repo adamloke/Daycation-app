b1f12f45703a8a91409a7de5cf5170feb2d71351
Adam Loke <adam.loke@hyperisland.se>
2020-07-03 11:02:07 +0200

    added articles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const articleObj = {
+  title: "Romai Fürdö",
+  lead: "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+};
+
+document.getElementById('article0Title').textContent = articleObj.title;
+document.getElementById('article0Lead').textContent = articleObj.lead;
+
+

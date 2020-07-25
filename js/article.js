8877044542c3d01a1370dc442395ad0889328d2c
Richard-Kalmar <richard-kalmar_student2020@wilder.school>
2020-07-25 19:20:27 +0200

    Map changed from Google maps to Leaflet. SCSS created for article page. Ready to merge with master

--- js/article.js
+++ js/article.js
@@ -1,29 +1,29 @@
-// Map function
+// MAP FUNCTION
 
-function initMap() {
-    let location = { lat: 47.195242, lng: 18.596261 };
-    let map = new google.maps.Map(document.getElementById("map"), { zoom: 6, center: location });
-    let marker = new google.maps.Marker({ position: location, map: map });
-}
+// Set the map
+let myMap = L.map("map").setView([47.195242, 18.596261], 13);
+// Create a style for the map
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibG9iby1kZWwtYm9zcXVlIiwiYSI6ImNrZDFobzhkajBjaGYyem53bmg2cTU1OWgifQ.qiriHL0rZUNLWvqLenS8SA'
+}).addTo(myMap);
+// Create markers to places of interest
+let marker = L.marker([47.195242, 18.596261]).addTo(myMap);
+
 
 // Travel options dropdown
 
 function dropdownFunction() {
-    console.log(document.getElementById("travel-dropdown").classList);
     document.getElementById("travel-dropdown").classList.add("show");
 }
 
 window.onclick = function(event) {
-    console.log(event.target.matches('.link'));
-    if (!document.getElementsByClassName("dropdown-content")[0].contains(event.target)) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        let i;
-        for (i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (!openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    if (!document.getElementsByClassName("drop-button")[0].contains(event.target)) {
+        document.getElementById("travel-dropdown").classList.remove("show");
     }
 }
 
@@ -44,6 +44,7 @@ function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("imgSlides");
     let dots = document.getElementsByClassName("dot");
+    console.log(slideIndex);
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
@@ -55,3 +56,5 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
+
+// Review section functions

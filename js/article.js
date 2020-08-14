6df9b6c40e8a508b74609a8279775a3313179b6e
Richard-Kalmar <richard-kalmar_student2020@wilder.school>
2020-08-14 14:53:02 +0200

    I created the other article pages but they are still empty. Finished the commenting on article.js. Filled the first artcle page with real content. The only thing left is to optimalise the marker creation

--- js/article.js
+++ js/article.js
@@ -37,48 +37,64 @@ window.onclick = function(event) {
 }
 
 // Carousel without Bootstrap
-
+//Declare slide index variable that can be used in the following functions
 let slideIndex = 1;
 showSlides(slideIndex);
-
+//Declare a function for the arrow navigation
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
-
+//Declare a function for dot navigation
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
-
+//The carouse function
 function showSlides(n) {
-    let i;
+    //Get the images and the dots by their class names
     let slides = document.getElementsByClassName("imgSlides");
     let dots = document.getElementsByClassName("dot");
+    //This if statement assures that the carouse goes around if user is 
+    //clicking on the next button
     if (n > slides.length) { slideIndex = 1 }
+    //This if statement is doing the same with the prev button
     if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
+    //This two loops sets the default to the carouse. Images not visible
+    for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
+    //this two lines sets the visibility by the slideIndex and adds the active class
+    //to the appropriate dot.
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
 }
 
 // REVIEW SECTION
 // Star rating
+//Declare the rate variable and give it a default value so it won't be 0 if the user
+//don' click on the stars
 let rate = 3;
+//Get the stars by their class names in an array
 const stars = document.getElementsByClassName("rating-star");
+//Create a new array for the IDs of the stars, iterate over the previous array
+//and push the IDs into the new array
 const starId = [];
 for (let i = 0; i < stars.length; i++) {
     starId.push(stars[i].id)
 }
+//With the forEach method add an event listener to every star
 starId.forEach(function(element) {
     document.getElementById(element).addEventListener("click", function() {
+        //Get the ID that the user clicked on
         let marker = document.getElementById(element).id;
+        //Set a counter that is used in the loops to fill or empty the stars based
+        //on the click event
         let counter = starId.indexOf(marker);
+        //set the rate value to the users choice
         rate = counter + 1;
-        console.log(marker);
+        //This two loops are adding and removing classes to fill or empty the stars
         for (let i = stars.length; i > counter; i--) {
             stars[i - 1].classList.remove("fas")
             stars[i - 1].classList.add("far")
@@ -132,20 +148,29 @@ let displayReviews = function(reviewContainer) {
 document.getElementById("display").innerHTML = displayReviews(reviewContainer);
 
 //Submit reviews
-
+//Get the submit button by id
 let submit = document.getElementById("submit-button")
+    //Add the event listener
 submit.addEventListener("click", function() {
+    // Create a variable from the form that can be passed to a FormData object
     let post = document.forms.reviewForm;
+    //Create a new FormData object from the variable
     let formData = new FormData(post);
+    // Get the field contents with the get method and add them to appropriate variables
+    // also use the rate from the rating function
     let user = formData.get("user");
     let currentRate = rate;
     let currentReview = formData.get("review");
+    //Create a new review object and pass the previously declared variables to its keys
     let reviewObject = { name: "", rating: 0, review: "" };
     reviewObject.name = user;
     reviewObject.rating = currentRate;
     reviewObject.review = currentReview;
+    //Add the new object to the container array
     reviewContainer.push(reviewObject);
+    //Display the new review as soon as it is submitted
     document.getElementById("display").innerHTML = displayReviews(reviewContainer);
+    //Reset the forms fields
     document.getElementById("reviewForm").reset();
 });
 
@@ -187,7 +212,7 @@ getTime = function() {
 }
 
 getCost = function() {
-    return `
+        return `
     <h4>Estimated cost:</h4>
     <ul>
     <li>Train: 1.1 h</li>
@@ -196,13 +221,16 @@ getCost = function() {
     <li>GreenGo: 0.25 h</li>
     </ul>
     `
-}
-
+    }
+    //Estimated travel time and cost is displayed
 document.getElementById("travel-time").innerHTML = getTime();
 document.getElementById("travel-cost").innerHTML = getCost();
 
 //Navbar
+//Get the elemnt by ID and create a variable
 const menuIcon = document.getElementById("menu-icon")
+    //Add an event listener to the variable what adds and removes the responsive class
+    //to open the menu on small screens.
 menuIcon.addEventListener("click", () => {
     let x = document.getElementById("topNav");
     if (x.className === "navbar") {
